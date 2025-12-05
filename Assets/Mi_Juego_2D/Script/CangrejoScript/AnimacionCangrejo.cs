985f3dff71196c5ca2daae81f560096759c0ea20
AmfDAM06 <alvaromartinezflores06@gmail.com>
2025-12-05 14:34:49 +0100

    Animacion de cangrejos arregladas + enemigos funcionales

--- Assets/Mi_Juego_2D/Script/CangrejoScript/AnimacionCangrejo.cs
+++ Assets/Mi_Juego_2D/Script/CangrejoScript/AnimacionCangrejo.cs
@@ -3,31 +3,55 @@ using UnityEngine;
 public class AnimacionCangrejo : MonoBehaviour
 {
     private GameObject cangrejo;
-    private GameObject crab_idle;
+    private GameObject crab_idle; // El hijo que tiene el Sprite y el Animator
     private Animator animacion;
     private Vector3 ultimaPosicion;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
     void Start()
     {
-
         cangrejo = transform.parent.gameObject;
+
+        // Buscamos al hijo visual (asegúrate de que se llama así en la jerarquía)
         crab_idle = cangrejo.transform.Find("crab-idle-1").gameObject;
-        animacion = crab_idle.GetComponent<Animator>();
-        ultimaPosicion = crab_idle.transform.position;
+
+        if (crab_idle != null)
+        {
+            animacion = crab_idle.GetComponent<Animator>();
+            ultimaPosicion = crab_idle.transform.position;
+        }
+        else
+        {
+            Debug.LogError("No encuentro el hijo 'crab-idle-1' en el enemigo antiguo.");
+        }
     }
 
-    // Update is called once per frame
     void Update()
     {
-        AnimarCangrejo();
+        if (animacion != null)
+        {
+            AnimarCangrejo();
+        }
     }
 
     private void AnimarCangrejo()
     {
         Vector3 posicionActual = crab_idle.transform.position;
-        float diferenciaX = posicionActual.x - ultimaPosicion.x;
 
-        if (diferenciaX > 0.001f || diferenciaX < -0.001f)
-            animacion.Play("CangrejoAndando");
+        // Calculamos cuánto se ha movido desde el último frame
+        float diferenciaX = Mathf.Abs(posicionActual.x - ultimaPosicion.x);
+
+        // Si se mueve algo (más de 0.001 unidades), activamos "andando"
+        if (diferenciaX > 0.001f)
+        {
+            animacion.SetBool("CangrejoAndando", true);
+        }
+        else
+        {
+            // Si está quieto, desactivamos "andando" para que vuelva a Idle
+            animacion.SetBool("CangrejoAndando", false);
+        }
+
+        // ¡IMPORTANTE! Actualizamos la posición para la siguiente comprobación
+        ultimaPosicion = posicionActual;
     }
 }
--- Assets/Mi_Juego_2D/Script/EnemigosComunScript/ControlEnemigoPersigue.cs
+++ Assets/Mi_Juego_2D/Script/EnemigosComunScript/ControlEnemigoPersigue.cs
@@ -1,47 +1,95 @@
 using UnityEngine;
 
-
-
 public class ControlEnemigoPersigue : MonoBehaviour
 {
+    [Header("Configuración de Movimiento")]
     public float velocidad;
-
-    private GameObject enemy;
-
-    private GameObject jugador;
-
-    public Vector3 posicionDestino;
-    public Vector3 posicionEnemigo;
-    public Vector3 posicionJugador;
-    private Rigidbody2D fisicaJugador;
-
     public float minimoX;
     public float maximoX;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [Header("Configuración de IA")]
+    public float distanciaVision = 5.0f;
+
+    [Header("Referencias")]
+    private GameObject enemy;
+    private Transform objetivoA_Perseguir;
+    private SpriteRenderer spriteRenderer;
+    private Animator miAnimator;
+
+    private Vector3 posicionDestino;
+
     void Start()
     {
-        jugador = GameObject.FindGameObjectWithTag("Player");
-        fisicaJugador = jugador.GetComponentInChildren<Rigidbody2D>();
-        posicionJugador = jugador.transform.position;
         enemy = transform.parent.gameObject;
-        Debug.Log("Posicion jugador: " + posicionJugador);
+
+        GameObject jugadorHijo = GameObject.FindGameObjectWithTag("Player_Idle");
+
+        if (jugadorHijo != null)
+        {
+            objetivoA_Perseguir = jugadorHijo.transform;
+        }
+        else
+        {
+            Debug.LogError("ERROR: No encuentro 'Player_Idle'.");
+        }
+
+        spriteRenderer = enemy.GetComponentInChildren<SpriteRenderer>();
+        miAnimator = enemy.GetComponentInChildren<Animator>();
+
+        if (miAnimator == null)
+        {
+            Debug.LogError("¡SOCORRO! No encuentro el componente Animator en el enemigo ni en sus hijos.");
+        }
+        else
+        {
+            Debug.Log("¡Genial! He encontrado el Animator y está listo.");
+        }
     }
 
-    // Update is called once per frame
-
-    private void FixedUpdate()
+    void Update()
     {
-        moverEnemigo();
-        posicionJugador = fisicaJugador.position;
-        Debug.Log("Posicion jugador: " + posicionJugador);
+        if (objetivoA_Perseguir == null) return;
 
+        float distanciaActual = Vector3.Distance(enemy.transform.position, objetivoA_Perseguir.position);
+
+        if (distanciaActual < distanciaVision)
+        {
+            moverEnemigo();
+            gestionarOrientacion();
+
+            if (miAnimator != null) miAnimator.SetBool("CangrejoAndando", true);
+        }
+        else
+        {
+            if (miAnimator != null) miAnimator.SetBool("CangrejoAndando", false);
+        }
     }
 
     private void moverEnemigo()
     {
-        float x = posicionJugador.x < minimoX ? minimoX : posicionJugador.x > maximoX ? maximoX : posicionJugador.x;
-        posicionDestino = new Vector3(x, enemy.transform.position.y, enemy.transform.position.z);
+        float xClamped = Mathf.Clamp(objetivoA_Perseguir.position.x, minimoX, maximoX);
+        posicionDestino = new Vector3(xClamped, enemy.transform.position.y, enemy.transform.position.z);
         enemy.transform.position = Vector3.MoveTowards(enemy.transform.position, posicionDestino, velocidad * Time.deltaTime);
+    }
+
+    private void gestionarOrientacion()
+    {
+        if (posicionDestino.x > enemy.transform.position.x)
+        {
+            spriteRenderer.flipX = true;
+        }
+        else if (posicionDestino.x < enemy.transform.position.x)
+        {
+            spriteRenderer.flipX = false;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (enemy != null)
+        {
+            Gizmos.color = Color.yellow;
+            Gizmos.DrawWireSphere(enemy.transform.position, distanciaVision);
+        }
     }
 }
